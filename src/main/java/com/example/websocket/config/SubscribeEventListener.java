d6a8a482ef2df445550269c71efc54e417f2523e
lihaoyang <lihaoyang@huayugroup360.com>
2019-01-21 17:40:02 +0800

    订阅监听

---
+++ src/main/java/com/example/websocket/config/SubscribeEventListener.java
@@ -0,0 +1,30 @@
+package com.example.websocket.config;
+
+/**
+ * @author lihy
+ * @version 2019/1/21
+ */
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionSubscribeEvent;
+
+/**
+ * 订阅监听
+ */
+@Component
+public class SubscribeEventListener implements ApplicationListener {
+
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (event instanceof SessionSubscribeEvent) {
+            SessionSubscribeEvent sessionSubscribeEvent = (SessionSubscribeEvent) event;
+            StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(sessionSubscribeEvent.getMessage());
+            System.out.println(headerAccessor.getSessionAttributes().get("sessionId").toString());
+        }
+
+    }
+}
